c1b823bb5071f579326cc234972d0c0d41f0d6fb
JacobLorSHH <144691606+JacobLorSHH@users.noreply.github.com>
2023-10-27 21:31:22 +0000

    Working Python; Not tested php and html

--- assignment.py
+++ assignment.py
@@ -1,1 +1,37 @@
-
+#Options
+#Sizes and corresponding values
+possible_sizes = ('single slice', 'xs', 's', 'm', 'l', 'xl', 'xxl')
+size_pricing = (2, 4, 5, 7, 12, 14, 16)
+possible_toppings = (0, 1, 2, 3, 4)
+topping_pricing = (0, 1, 1.75, 2.5, 3.35)
+#UI
+not_satisfied = True
+total = 0
+while not_satisfied:
+    print('y or n')
+    want_pizza = str(input('Do you want a pizza? '))
+    if want_pizza == 'n':
+        break
+    else:
+        #Finding size
+        while True:
+            size = str(input('What size of pizza would you like?'))
+            if size in possible_sizes:
+                total += size_pricing[possible_sizes.index(size)]
+                break #this is the escape hatch
+            else:
+                print('Not a possible size!')
+                print('Here are the sizes availabe: ')
+                print(*possible_sizes, sep=', ')
+        while True:
+            try:
+                num_of_toppings = int(input('How many toppings do you want? '))
+                if num_of_toppings in possible_toppings:
+                    total += topping_pricing[possible_toppings.index(num_of_toppings)]
+                    break
+                else:
+                    print("You can't have negative toppings or more than 4")
+            except ValueError:
+                print('Must be an Integer!')
+        
+print(total * 1.13)
--- handler.php
+++ handler.php
@@ -10,12 +10,33 @@
   <body>
     
     <?php
-    $myVariable = "";
-    if ( isset( $_POST['myVariable'] ) ){
-      $myVariable = $_POST['myVariable'];
+    $Cost = 0;
+    if ( isset( $_POST['Size'] ) and isset( $_POST['Number_Of_Topping'])){
+      $Size = $_POST['Size'];
+      $Number_Of_Topping = $_POST['Number_Of_Topping'];
+      if ($Number_Of_Topping > 0 and $Number_Of_Topping <= 4){
+        $Topping_Cost = ($Number_Of_Topping - 1) * 0.75 + 1;
+        if ($Size == 'Large'){
+          $Cost += 6;
+          echo $Topping_Cost + $Cost;
+        }
+        elseif ($Size == 'Extra Large'){
+          $Cost += 10;
+          echo $Topping_Cost + $Cost;
+        }
+        else{
+          echo 'Size not available';
+          echo 'Our available Sizes are Large and Extra Large';
+        }
+        
+      }
+      else{
+        echo 'You can only have toppings between 0 and 4!';
+      }
     }
-    echo "<h1>My Program</h1>\n";
-    echo "<p>My Variable is = ".$myVariable."</p>\n";
+    else{
+      echo 'You must fill out both textfields before ordering!';
+    }
     ?>
     
   </body>
