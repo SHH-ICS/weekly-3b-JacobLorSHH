a366b98d48624b7fca918a7b218a5dbb96d2d460
JacobLorSHH <144691606+JacobLorSHH@users.noreply.github.com>
2023-11-02 13:29:22 +0000

    In Progress

--- handler.php
+++ handler.php
@@ -12,30 +12,18 @@
     <?php
     $Cost = 0;
     if ( isset( $_POST['Size'] ) and isset( $_POST['Number_Of_Topping'])){
-      $Size = $_POST['Size'];
       $Number_Of_Topping = $_POST['Number_Of_Topping'];
-      if ($Number_Of_Topping > 0 and $Number_Of_Topping <= 4){
-        $Topping_Cost = ($Number_Of_Topping - 1) * 0.75 + 1;
-        if ($Size == 'Large'){
-          $Cost += 6;
-          echo $Topping_Cost + $Cost;
-        }
-        elseif ($Size == 'Extra Large'){
-          $Cost += 10;
-          echo $Topping_Cost + $Cost;
-        }
-        else{
-          echo 'Size not available';
-          echo 'Our available Sizes are Large and Extra Large';
-        }
-        
+      $Size = $_POST['Size'];
+      if ($Size <= 4 and $Size >= 0){
+        $Topping_Cost = ((Number_Of_Topping - 1) * 0.75) + 1;
+        if $Size == ""
       }
       else{
-        echo 'You can only have toppings between 0 and 4!';
+        echo "You can only have toppings from 0-4!";
       }
     }
-    else{
-      echo 'You must fill out both textfields before ordering!';
+    else {
+      echo "you must fill out both inputfields";
     }
     ?>
     
